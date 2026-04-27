06f66d23cb54012275a54e421263fa8701a003d1
LuisBarbosaGit <luisbarbosa.santos@outlook.com>
2026-04-27 16:35:17 -0300

    Remove warmup function and its import from server initialization to streamline startup process.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import Fastify from "fastify";
 import { mainRoutes } from "./routes.js";
 import { initializeVectorStore } from "./utils/searchByVector.js";
-import { warmup } from "./warmup/warmup.js";
 
 const app = Fastify({
   logger: true,
@@ -11,7 +10,6 @@ const start = async () => {
   try {
     app.register(mainRoutes);
     initializeVectorStore();
-    warmup();
     await app.listen({ host: "0.0.0.0", port: 3000 });
     console.log(`API listening on ${3000}`);
   } catch (err) {
